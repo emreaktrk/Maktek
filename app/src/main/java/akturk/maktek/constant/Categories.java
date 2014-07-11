d24fca28d471a635fbc3f70b67a54f00e924094c
Emre Akturk <akturke@netas.com.tr>
2014-07-11 10:53:28 +0300

    Categories developing.

--- app/src/main/java/akturk/maktek/constant/Categories.java
+++ app/src/main/java/akturk/maktek/constant/Categories.java
@@ -1,12 +1,28 @@
 package akturk.maktek.constant;
 
-public class Categories {
-    public static final int[] MACHINE_TOOLS = {1,2,3,5,6,11,14};
-    public static final int[] METAL_PROCESSING_TECHNOLOGIES = {7,8,9};
-    public static final int[] TOOLS = {12};
-    public static final int[] METROLOGY_AND_MEASUREMENT = {4};
-    public static final int[] QUALITY_CONTROL_AND_TEST_EQUIPMENT = {4};
-    public static final int[] INTERNATIONAL_PARTICIPATIONS_AND_OTHER = {1,11};
-    public static final int[] CADCAM_AND_CONTROL_SYSTEMS = {14};
-    public static final int[] OTHERS = {13};
+public enum Categories {
+    MACHINE_TOOLS("MACHINE TOOLS", Saloons.MACHINE_TOOLS),
+    METAL_PROCESSING_TECHNOLOGIES("METAL PROCESSING TECHNOLOGIES", Saloons.METAL_PROCESSING_TECHNOLOGIES),
+    TOOLS("TOOLS", Saloons.TOOLS),
+    METROLOGY_AND_MEASUREMENT("METROLOGY AND MEASUREMENT", Saloons.METROLOGY_AND_MEASUREMENT),
+    QUALITY_CONTROL_AND_TEST_EQUIPMENT("QUALITY CONTROL AND TEST EQUIPMENT", Saloons.QUALITY_CONTROL_AND_TEST_EQUIPMENT),
+    INTERNATIONAL_PARTICIPATIONS_AND_OTHER("INTERNATIONAL PARTICIPATIONS AND OTHER", Saloons.INTERNATIONAL_PARTICIPATIONS_AND_OTHER),
+    CADCAM_AND_CONTROL_SYSTEMS("CADCAM AND CONTROL SYSTEMS", Saloons.CADCAM_AND_CONTROL_SYSTEMS),
+    OTHERS("OTHERS", Saloons.OTHERS);
+
+    private String mName;
+    private String[] mSaloons;
+
+    private Categories(String name, String[] saloons) {
+        this.mName = name;
+        this.mSaloons = mSaloons;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String[] getSaloons() {
+        return mSaloons;
+    }
 }
--- app/src/main/java/akturk/maktek/constant/SingleShotID.java
+++ app/src/main/java/akturk/maktek/constant/SingleShotID.java
@@ -1,7 +1,7 @@
 package akturk.maktek.constant;
 
 
-public class SingleShotID {
+public final class SingleShotID {
     public static final int SHOWCASE_SINGLESHOT_TITLESTRIP = 0;
     public static final int SHOWCASE_SINGLESHOT_HOME = 1;
     public static final int SHOWCASE_SINGLESHOT_PULL_DOWN_TO_REFRESH = 2;
