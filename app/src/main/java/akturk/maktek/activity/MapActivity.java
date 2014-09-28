ae2a6e16313de9643e93da35300587853aaed3a1
Emre Akturk <emreaktrk@hotmail.com>
2014-09-28 07:10:54 +0300

    Overlay image refactored.

--- app/src/main/java/akturk/maktek/activity/MapActivity.java
+++ app/src/main/java/akturk/maktek/activity/MapActivity.java
@@ -56,7 +56,7 @@ public final class MapActivity extends BaseActivity {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_activity_map)).getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null)
                 setUpMap();
