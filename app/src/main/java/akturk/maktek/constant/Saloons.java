0bfe3843ab4e6b2ed8328bd053e8f74ff4311c02
Emre Akturk <emreaktrk@hotmail.com>
2014-09-30 22:36:30 +0300

    Categories changed.

--- app/src/main/java/akturk/maktek/constant/Saloons.java
+++ app/src/main/java/akturk/maktek/constant/Saloons.java
@@ -2,8 +2,8 @@ package akturk.maktek.constant;
 
 public final class Saloons {
     public static final String[] ALL = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"};
-    public static final String[] MACHINE_TOOLS = {"1", "2", "3", "5", "6", "11", "14"};
-    public static final String[] METAL_PROCESSING_TECHNOLOGIES = {"7", "8", "9"};
+    public static final String[] MACHINE_TOOLS = {"2", "3", "5", "6", "10"};
+    public static final String[] METAL_PROCESSING_TECHNOLOGIES = {"7", "7A", "8", "9"};
     public static final String[] TOOLS = {"12"};
     public static final String[] METROLOGY_MEASUREMENT_QUALITY_CONTROL_AND_TEST_EQUIPMENT = {"4"};
     public static final String[] INTERNATIONAL_PARTICIPATIONS_AND_OTHER = {"1", "11"};
